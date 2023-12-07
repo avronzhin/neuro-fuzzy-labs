f2d5f04fdd2b869254907e48aa60d5ea4b1f5a56
aleksejronzhin <aleksej.ronzhin.rsreu@gmail.com>
2023-12-07 23:40:32 +0300

    Finished lab6

--- lab6/main.py
+++ lab6/main.py
@@ -3,66 +3,45 @@ import numpy as np
 import skfuzzy as fuzz
 from skfuzzy import control as ctrl
 
+# Лингвистические переменные
 attendance = ctrl.Antecedent(np.arange(0, 17, 1), 'Посещаемость')
 performance = ctrl.Antecedent(np.arange(0, 9, 1), 'Результативность')
-assessment = ctrl.Consequent(np.arange(2, 6, 1), 'Оценка')
+assessment = ctrl.Consequent(np.arange(2, 5.1, 0.1), 'Оценка')
 
-attendance['Хорошая'] = fuzz.trapmf(attendance.universe, [12, 16, 16, 16])
-attendance['Плохая'] = fuzz.trapmf(attendance.universe, [0, 0, 1, 4])
+# Термы
+# Посещаемость
+attendance['Хорошая'] = fuzz.smf(attendance.universe, 5, 16)
+attendance['Плохая'] = fuzz.zmf(attendance.universe, 0, 7)
 
-# Задать нормальные функции принадлежности
-performance['Высокая'] = fuzz.gaussmf(performance.universe, 0, 1.5)
-performance['Средняя'] = fuzz.trapmf(performance.universe, [3, 4, 5, 6])
-performance['Низкая'] = fuzz.gaussmf(performance.universe, 5, 1.5)
+# Результативность
+performance['Высокая'] = fuzz.smf(performance.universe, 3, 7)
+performance['Средняя'] = fuzz.pimf(performance.universe, 1, 3, 4, 5)
+performance['Низкая'] = fuzz.zmf(performance.universe, 0, 3)
 
-assessment['Отличная'] = fuzz.trimf(assessment.universe, [4, 5, 5])
-assessment['Нормальная'] = fuzz.trimf(assessment.universe, [3, 4, 4])
-assessment['Ужасная'] = fuzz.trimf(assessment.universe, [2, 2, 3])
+# Оценка
+assessment['Отличная'] = fuzz.smf(assessment.universe, 4, 4.9)
+assessment['Нормальная'] = fuzz.pimf(assessment.universe, 2.7, 3.5, 3.7, 4.3)
+assessment['Ужасная'] = fuzz.zmf(assessment.universe, 2, 3.4)
 
 attendance.view()
 performance.view()
 assessment.view()
-
 # assessment['Отличная'].view()
 
-# Нет возможности задавать всес правила
-rule1 = ctrl.Rule(performance['Низкая'] & ~attendance['Хорошая'], assessment['Ужасная'])
-rule2 = ctrl.Rule(performance['Высокая'], assessment['Отличная'])
-rule3 = ctrl.Rule(performance['Средняя'] | attendance['Хорошая'], assessment['Нормальная'])
+# Правила
+rule1 = ctrl.Rule(performance['Высокая'] & ~attendance['Плохая'], assessment['Отличная'])
+rule2 = ctrl.Rule(performance['Низкая'], assessment['Ужасная'])
+rule3 = ctrl.Rule(performance['Средняя'], assessment['Нормальная'])
 
 control_system = ctrl.ControlSystem([rule1, rule2, rule3])
 
 control_system_simulation = ctrl.ControlSystemSimulation(control_system)
 
-control_system_simulation.input['Посещаемость'] = 16 # Сколько пар посетил студент
-control_system_simulation.input['Результативность'] = 8 # Сколько лаб сдал студент
+control_system_simulation.input['Посещаемость'] = 8  # Сколько пар посетил студент
+control_system_simulation.input['Результативность'] = 4  # Сколько лаб сдал студент
 
 control_system_simulation.compute()
 
 print(control_system_simulation.output['Оценка'])
 
 assessment.view(sim=control_system_simulation)
-
-unsampled1 = np.arange(0, 9, 1)
-unsampled2 = np.arange(0, 17, 1)
-
-x, y = np.meshgrid(unsampled1, unsampled2)
-
-z = np.zeros_like(x)
-for i in range(17):
-    for j in range(9):
-        control_system_simulation.input['Посещаемость'] = x[i, j]
-        control_system_simulation.input['Результативность'] = y[i, j]
-        control_system_simulation.compute()
-        z[i, j] = control_system_simulation.output['Оценка']
-
-fig = plt.figure(figsize=(8, 8))
-ax = fig.add_subplot(111, projection='3d')
-surf = ax.plot_surface(x, y, z, rstride=1, cstride=1, cmap='viridis', linewidth=0.4, antialiased=True)
-
-cset = ax.contourf(x, y, z, zdir='z', offset=-2.5, cmap='viridis', alpha=0.5)
-cset = ax.contourf(x, y, z, zdir='x', offset=30, cmap='viridis', alpha=0.5)
-cset = ax.contourf(x, y, z, zdir='y', offset=30, cmap='viridis', alpha=0.5)
-
-ax.view_init(50, 200)
-plt.show()

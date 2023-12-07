437a0ca5736984dd118fd324016c2990587feca1
aleksejronzhin <aleksej.ronzhin.rsreu@gmail.com>
2023-12-07 23:54:43 +0300

    Added 3d graphic

--- lab6/main.py
+++ lab6/main.py
@@ -1,7 +1,9 @@
-import matplotlib.pyplot as plt
 import numpy as np
 import skfuzzy as fuzz
 from skfuzzy import control as ctrl
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
 
 # Лингвистические переменные
 attendance = ctrl.Antecedent(np.arange(0, 17, 1), 'Посещаемость')
@@ -32,8 +34,9 @@ assessment.view()
 rule1 = ctrl.Rule(performance['Высокая'] & ~attendance['Плохая'], assessment['Отличная'])
 rule2 = ctrl.Rule(performance['Низкая'], assessment['Ужасная'])
 rule3 = ctrl.Rule(performance['Средняя'], assessment['Нормальная'])
+rule4 = ctrl.Rule(performance['Высокая'] & attendance['Плохая'], assessment['Нормальная'])
 
-control_system = ctrl.ControlSystem([rule1, rule2, rule3])
+control_system = ctrl.ControlSystem([rule1, rule2, rule3, rule4])
 
 control_system_simulation = ctrl.ControlSystemSimulation(control_system)
 
@@ -45,3 +48,37 @@ control_system_simulation.compute()
 print(control_system_simulation.output['Оценка'])
 
 assessment.view(sim=control_system_simulation)
+
+# Генерация значений для построения поверхности
+resultiveness_values = np.arange(0, 9, 1)
+attendance_values = np.arange(0, 17, 1)
+
+# Создание сетки для значений Результативность и Посещаемость
+resultiveness_mesh, attendance_mesh = np.meshgrid(resultiveness_values, attendance_values)
+
+# Вычисление значений для Оценки с использованием выделенной нечеткой системы
+assessment_values = np.zeros_like(resultiveness_mesh)
+for i in range(len(resultiveness_values)):
+    for j in range(len(attendance_values)):
+        control_system_simulation.input['Посещаемость'] = attendance_values[j]
+        control_system_simulation.input['Результативность'] = resultiveness_values[i]
+        control_system_simulation.compute()
+        # Используйте i и j в обратном порядке при присвоении значений
+        assessment_values[j, i] = control_system_simulation.output['Оценка']
+
+# Построение 3D графика
+fig = plt.figure(figsize=(10, 8))
+ax = fig.add_subplot(111, projection='3d')
+
+# Построение поверхности
+surface = ax.plot_surface(resultiveness_mesh, attendance_mesh, assessment_values, cmap='viridis')
+
+# Добавление подписей осей
+ax.set_xlabel('Результативность')
+ax.set_ylabel('Посещаемость')
+ax.set_zlabel('Оценка')
+
+# Добавление цветовой шкалы
+fig.colorbar(surface, ax=ax, shrink=0.5, aspect=10)
+
+plt.show()
